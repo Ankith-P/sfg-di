4e47cc92bcc3695d43a82d2f398d0e402c37a9fe
ankith.prakash <ankith.prakash@metricstream.com>
2021-09-14 17:23:58 +0530

    Adding code related to lecture 31-Simple Create Spring DI Example Project

---
+++ src/main/java/guru/spring/framework/sfgdiank/SfgDiAnkApplication.java
@@ -0,0 +1,13 @@
+package guru.spring.framework.sfgdiank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDiAnkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDiAnkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/spring/framework/sfgdiank/SfgDiAnkApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring.framework.sfgdiank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiAnkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
